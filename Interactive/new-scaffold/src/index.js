4a5776226a011815b657de9107a64a283edd675e
Ian Lyons <ian.thomas.lyons@gmail.com>
2019-03-05 11:38:07 -0600

    updates

--- Interactive/new-scaffold/src/index.js
+++ Interactive/new-scaffold/src/index.js
@@ -1,44 +1,60 @@
 
 const domReady = require('domready');
 
-import {geoPath, geoAlbersUsa} from 'd3-geo';
 import {select} from 'd3-selection';
 import {interpolateInferno} from 'd3-scale-chromatic';
-import {scaleLinear} from 'd3-scale';
+import {scaleOrdinal, scaleTime, scaleLinear} from 'd3-scale';
+import {timeFormat} from 'd3-time-format';
+import {line} from 'd3-shape';
+import {axisBottom, axisRight} from 'd3-axis';
+import {annotation, annotationCalloutCircle} from 'd3-svg-annotation';
+var d3 = require("d3");
 
 domReady(() => {
-  Promise.all(["../app/data/cbo.json"]).then(data => data.json())
-    .then(data => myVis(data));
-});
+    fetch('data/cbo.json')
+    .then(response => response.json())
+    .then(data => Visify(data));
+    });
 
-function myVis(data) {
+function Visify(data) {
   // portrait
-    const width = 5000;
+    const width = 1000;
     const height = 36 / 24 * width;
 
-    var margin = {left: 75,
-                right: 50,
-                top: 50,
-                bottom: 75
+    var margin = {left: 20,
+                right: 20,
+                top: 20,
+                bottom: 20
                 };
+    const plotWidth = width - margin.left - margin.right;
+    const plotHeight = height - margin.bottom - margin.top;
 
-    var xScale = d3.scaleLinear()
-                        .domain([d3.min(dataset, function(d) { return d.year; }),
-                            d3.max(dataset, function(d) { return d.year; })])
-                        .range([padding, w - padding * 2])
-                        .nice();
-
-    var yScale = d3.scaleLinear()
-                        .domain([d3.min(dataset, function(d) { return d.amount; }),
-                            d3.max(dataset, function(d) { return d.amount; })])
-                        .range([h - padding, padding])
-                        .nice();
-
-    //Create SVG
-    var svg = d3.select("body")
+/*    var svg = select("body")
                 .append("svg")
-                .attr("width", w)
-                .attr("height", h);
+                .attr("width", width)
+                .attr("height", height); */
+
+    const svg = select('.vis-container')
+                    .attr('width', width)
+                    .attr('height', height)
+                    .append('g')
+                    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    var xScale = scaleLinear()
+                        .domain([d3.min(data, function(d) { return d.year; }),
+                            d3.max(data, function(d) { return d.year; })])
+                        .range([0, width])
+                        .nice();
+
+    var yScale = scaleLinear()
+                        .domain([d3.min(data, function(d) { return d.amount; }),
+                            d3.max(data, function(d) { return d.amount; })])
+                        .range([height, 0])
+                        .nice();
+
+    const color = d3.scaleOrdinal(d3.schemeDark2);
+    //Create SVG
+
 
   console.log(data)
   console.log('Hi! My name is Ian and Im changing things.')
