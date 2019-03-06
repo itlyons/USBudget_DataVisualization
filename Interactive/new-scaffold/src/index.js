b15ada1db3417763a723aa64d8ad0137b4c9f8ce
Ian Lyons <ian.thomas.lyons@gmail.com>
2019-03-05 18:44:23 -0600

    new data, axis style

--- Interactive/new-scaffold/src/index.js
+++ Interactive/new-scaffold/src/index.js
@@ -10,18 +10,25 @@ import {axisBottom, axisRight} from 'd3-axis';
 import {annotation, annotationCalloutCircle} from 'd3-svg-annotation';
 var d3 = require("d3");
 
+/*
 domReady(() => {
     fetch('data/cbo.json')
     .then(response => response.json())
     .then(data => Visify(data));
-    });
+});
+*/
+
+domReady(() => {
+        d3.csv('data/cboSpend.csv')
+        .then(data => Visify(data));
+        });
 
 function Visify(data) {
   // portrait
-    const width = 1000;
+    const width = 999;
     const height = 24 / 36 * width;
 
-    var margin = {left: 50,
+    var margin = {left: 40,
                 right: 20,
                 top: 20,
                 bottom: 20
@@ -37,34 +44,35 @@ function Visify(data) {
                     .attr('transform', `translate(${margin.left},${margin.top})`);
 
     var xScale = scaleLinear()
-                        .domain([d3.min(data, function(d) { return d.year; }),
-                            d3.max(data, function(d) { return d.year; })])
+                        .domain([d3.min(data, function(d) { return d.Year; }),
+                            d3.max(data, function(d) { return d.Year; })])
                         .range([0, plotWidth])
                         .nice();
 
     var yScale = scaleLinear()
-                        .domain([d3.min(data, function(d) { return d.amount; }),
-                            d3.max(data, function(d) { return d.amount; })])
+                        .domain([0,100])
                         .range([plotHeight, 0])
                         .nice();
 
     const color = d3.scaleOrdinal(d3.schemeDark2);
 
+    var yFormat = d3.axisBottom(xScale)
+                .tickFormat(d3.format("d"));
+
     svg.append('g')
         .attr('class', 'axis')
         .attr('transform', 'translate(10,' + plotHeight + ')')
-        .call(d3.axisBottom(xScale)
-        .tickFormat(d3.format("d")));
+        .call(yFormat);
+
+    var xFormat = d3.axisLeft(yScale)
+                .tickFormat(d3.format("1.0hyhyh7uuy6hp"));
 
     svg.append('g')
         .attr('class', 'axis')
         .attr('transform', 'translate(10,0)')
-        .call(d3.axisLeft(yScale)
-        .tickFormat(d3.format("$.0s"))
-    );
-
+        .call(xFormat);
 
   console.table(data)
   console.log()
-  
+
 }
