e99f2c0a64dd3538d2c3a86a891976231b08ab74
Ian Lyons <ian.thomas.lyons@gmail.com>
2019-03-21 20:52:42 -0500

    button text & listener

--- Interactive/new-scaffold/src/index.js
+++ Interactive/new-scaffold/src/index.js
@@ -1,6 +1,6 @@
 
 const domReady = require('domready');
-var d3Transition = require("d3-transition");
+var d3Transition = require('d3-transition');
 import {select, event} from 'd3-selection';
 import {interpolateInferno} from 'd3-scale-chromatic';
 import {scaleOrdinal, scaleTime, scaleLinear} from 'd3-scale';
@@ -9,12 +9,12 @@ import {line} from 'd3-shape';
 import {axisBottom, axisRight} from 'd3-axis';
 import {annotation, annotationCalloutCircle} from 'd3-svg-annotation';
 var annotate = require('d3-svg-annotation');
-var d3 = require("d3");
+var d3 = require('d3');
 
 domReady(() => {
-    fetch('data/cboSpend.json')
-    .then(response => response.json())
-    .then(data => Visify(data));
+    Promise.all(['data/cboSpend.json', 'data/TypesOfSpend.json']
+    .map(url => fetch(url).then(allData => allData.json())))
+    .then(allData => Visify(allData));
     });
 
 /*
@@ -23,8 +23,24 @@ domReady(() => {
         .then(data => Visify(data));
         });
 */
-function Visify(data) {
 
+
+function Visify(allData, whatChart) {
+    const chart_dict = [{'Chart':'Overview'}]
+    console.log(whatChart)
+    var allData = allData
+    var indexer = function indexizer(whatChart) {
+        if (whatChart === 'Overview')
+            {return 0}
+        if (whatChart === 'CBO Spending')
+            {return 1}
+        else {return 0};
+        }
+    var indexer = indexer(whatChart)
+    console.log(indexer)
+
+    var data = allData[indexer]
+    console.log(data)
     const width = 999;
     const height = 1.9/3 * width;
 
@@ -37,10 +53,12 @@ function Visify(data) {
     const plotWidth = width - margin.left - margin.right;
     const plotHeight = height - margin.bottom - margin.top;
 
+    var whatChart = 'overview'
     const svg = select('.vis-container')
                     .attr('width', width)
                     .attr('height', height)
                     .append('g')
+                    .attr('class', whatChart)
                     .attr('transform', `translate(${margin.left},${margin.top})`);
 
     var xScale = scaleLinear()
@@ -50,37 +68,40 @@ function Visify(data) {
                         .nice();
 
     var yScale = scaleLinear()
-                        .domain([0,1.60])
+                        .domain([0, d3.max(data, (d => d.pctgdp))])
                         .range([plotHeight, 0])
                         .nice();
 
-    var cats = ['Publicly Held Debt','Total Spending', 'Total Revenues'
-    , 'Social Security', 'Federal Healthcare Spending',
-    'Other Noninterest Spending', 'Net Interest']
+    var cats = ['Publicly Held Debt','Total Spending', 'Total Revenues',
+                'Discretionary', 'Mandatory', 'Net Interest']
+
+
+//    , 'Social Security', 'Federal Healthcare Spending',
+//    'Other Noninterest Spending', 'Net Interest']
 
     const color = d3.scaleOrdinal()
                     .domain(cats)
                     .range(d3.schemeDark2);
 
     var yFormat = d3.axisBottom(xScale)
-                .tickFormat(d3.format("d"));
+                .tickFormat(d3.format('d'));
 
     svg.append('g')
         .attr('class', 'axis')
         .attr('transform', 'translate(0,' + plotHeight + ')')
         .call(yFormat);
 
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
+    svg.append('text')
+        .attr('transform', 'rotate(-90)')
         .attr('class', 'ylabel')
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Percent of GDP");
+        .attr('y', 0 - margin.left)
+        .attr('x', 0 - (height / 2))
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text('Percent of GDP');
 
     var xFormat = d3.axisLeft(yScale)
-                .tickFormat(d3.format(".0%"));
+                .tickFormat(d3.format('.0%'));
 
     svg.append('g')
         .attr('class', 'axis')
@@ -93,40 +114,40 @@ function Visify(data) {
         .x(function(d) { return xScale(d.Year); })
         .y(function(d) { return yScale(d.pctgdp); });
 
-    const clipPath = svg.append("clipPath")
-            .attr("id", "clip")
-          .append("rect")
-            .attr("width", plotWidth)
-            .attr("height", plotHeight);
+    const clipPath = svg.append('clipPath')
+            .attr('id', 'clip')
+          .append('rect')
+            .attr('width', plotWidth)
+            .attr('height', plotHeight);
 
     // Only look at the toplines for now
-    var subset = [(data.filter(d => d.Category=="Publicly Held Debt")),
-                    (data.filter(d => d.Category=="Total Spending")),
-                    (data.filter(d => d.Category=="Total Revenues"))
+    var subset = [(data.filter(d => d.Category=='Publicly Held Debt')),
+                    (data.filter(d => d.Category=='Total Spending')),
+                    (data.filter(d => d.Category=='Total Revenues'))
                 ];
 
-    var lineHolder = svg.append("g").attr("transform", "translate(0,0)")
+    var lineHolder = svg.append('g').attr('transform', 'translate(0,0)')
         .attr('class', 'line-holder');
  //***********************************************************************//
-    lineHolder.selectAll(".line")
+    lineHolder.selectAll('.line')
         .data(subset)
             .enter()
-        .append("path")
+        .append('path')
         .attr('class', 'line')
-        .style('stroke', (d => color(d[0].Category)))
+        .style('stroke', (d => (color(d[0].Category))))
         .attr('clip-path', 'url(#clip)')
         .attr('d', (d => line(d)))
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mousemove", function(d) {
+        .on('mouseover', function() { focus.style('display', null); })
+        .on('mousemove', function(d) {
             var xPosition = d3.mouse(this)[0];
             var yPosition = d3.mouse(this)[1];
             var ttYear = (xScale.invert(xPosition)).toFixed(0)
             var ttPct = (yScale.invert(yPosition)*100).toFixed(0)
-            focus.attr("transform","translate(" +xPosition + "," + yPosition + ")")
+            focus.attr('transform','translate(' +xPosition + ',' + yPosition + ')')
                 .style('opacity', 1);
-            focus.select("text")
-                .text("Year: " + ttYear + ", Percent of GDP: " + ttPct + '%')
-                .attr("fill", "black")
+            focus.select('text')
+                .text('Year: ' + ttYear + ', Percent of GDP: ' + ttPct + '%')
+                .attr('fill', 'black')
                 .style('opacity', 1)
             })
          .on('mouseout', function() {
@@ -146,27 +167,28 @@ function Visify(data) {
                           .attr('y1', plotHeight)
                           .attr('x2', todayX)
                           .attr('y2', 0)
-                          .style("stroke-width", 1)
-                          .style("stroke", "red")
-                          .style("stroke-dasharray", ("3, 3"))
-                          .style("fill", "none");
+                          .style('stroke-width', 1)
+                          .style('stroke', 'red')
+                          .style('stroke-dasharray', ('3, 3'))
+                          .style('fill', 'none');
 // button --> change class of line you are choosing to not have delete class
 // remove lines, and func that undoes it
 
+    var annos = svg.append('g').attr('class', 'random-text');
     // Add labels for 2019 marker
-    svg.append("text")
+    annos.append('text')
             .attr('class', 'annotation')
-            .attr("x", todayX+25)
-            .attr("y", plotHeight*0.35)
-            .attr("text-anchor", "right")
-            .text("CBO Projection -->");
+            .attr('x', todayX+25)
+            .attr('y', plotHeight*0.35)
+            .attr('text-anchor', 'right')
+            .text('CBO Projection -->');
 
-    svg.append("text")
+    annos.append('text')
             .attr('class', 'annotation')
-            .attr("x", todayX-90)
-            .attr("y", plotHeight*0.35)
-            .attr("text-anchor", "left")
-            .text("<-- Actual");
+            .attr('x', todayX-90)
+            .attr('y', plotHeight*0.35)
+            .attr('text-anchor', 'left')
+            .text('<-- Actual');
 
     var curtain = svg.append('rect')
         .attr('class', 'curtain')
@@ -183,14 +205,14 @@ function Visify(data) {
         .attr('x', plotWidth*2);
 
     // Add chart title
-    svg.append("text")
+    annos.append('text')
         .attr('class', 'chart-title')
-        .attr("x", (width / 2))
-        .attr("y", 0 - (margin.top / 2))
-        .attr("text-anchor", "middle")
-        .style("font-size", "24px")
-        .style("text-decoration", "bold")
-        .text("Revenues Stagnate, and Debt Grows");
+        .attr('x', (width / 2))
+        .attr('y', 0 - (margin.top / 2))
+        .attr('text-anchor', 'middle')
+        .style('font-size', '24px')
+        .style('text-decoration', 'bold')
+        .text('Revenues Stagnate, and Debt Grows');
 
 
     // https://bl.ocks.org/d3noob/c506ac45617cf9ed39337f99f8511218
@@ -206,65 +228,70 @@ function Visify(data) {
             return d3.axisLeft(yScale)
                 .ticks(5)}
           // add the X gridlines
-        svg.append("g")
-              .attr("class", "grid")
-              .attr("transform", "translate(0," + plotHeight + ")")
+        svg.append('g')
+              .attr('class', 'grid')
+              .attr('transform', 'translate(0,' + plotHeight + ')')
               .call(make_x_gridlines()
                   .tickSize(-plotHeight)
-                  .tickFormat(""));
+                  .tickFormat(''));
           // add the Y gridlines
-        svg.append("g")
-            .attr("class", "grid")
-            .attr("transform", "translate(0," + 0 + ")")
+        svg.append('g')
+            .attr('class', 'grid')
+            .attr('transform', 'translate(0,' + 0 + ')')
             .call(make_y_gridlines()
                 .tickSize(-plotWidth)
-                .tickFormat(""));
+                .tickFormat(''));
     }
     make_grid();
 
 
 
-      var focus = svg.append("g")
-          .attr("class", "focus")
-          .style("display", "none")
+      var focus = svg.append('g')
+          .attr('class', 'focus')
+          .style('display', 'none')
           ;
 
-      var focusX = focus.append("line")
-          .attr("class", "x-hover-line hover-line")
-          .attr("y1", 0)
-          .attr("y2", plotHeight);
+      var focusX = focus.append('line')
+          .attr('class', 'x-hover-line hover-line')
+          .attr('y1', 0)
+          .attr('y2', plotHeight);
 
-      var focusY = focus.append("line")
-          .attr("class", "y-hover-line hover-line")
-          .attr("x1", -plotWidth)
-          .attr("x2", 0);
+      var focusY = focus.append('line')
+          .attr('class', 'y-hover-line hover-line')
+          .attr('x1', -plotWidth)
+          .attr('x2', 0);
 
-      var focusCirc = focus.append("circle")
-          .attr("r", 5.0);
+      var focusCirc = focus.append('circle')
+          .attr('r', 5.0);
 
-      var focusText = focus.append("text")
-            .attr("x", 15)
-            .attr("dy", ".31em")
+      var focusText = focus.append('text')
+            .attr('x', 15)
+            .attr('dy', '.31em')
             .style('opacity', 0);
+
+
+
+
+
 
 //****************** <LEGEND TIME> ******************
 //***************************************************
-    var legend = svg.append("g")
-              .attr("class","legend")
-              .attr("transform","translate(50,50)")
-              .style("font-size","12px");
+    var legend = svg.append('g')
+              .attr('class','legend')
+              .attr('transform','translate(50,50)')
+              .style('font-size','12px');
 
     // Add the legend's colored boxes.
     legend.selectAll('rect')
         .data(color.domain())
         .enter()
-        .append("rect")
+        .append('rect')
           .attr('class', 'legendBox')
-          .attr("x", plotWidth *.01)
-          .attr("y", function(d, i){ return i *  20;})
-          .attr("width", 18)
-          .attr("height", 18)
-          .style("fill", function(d) {
+          .attr('x', plotWidth *.01)
+          .attr('y', function(d, i){ return i *  20;})
+          .attr('width', 18)
+          .attr('height', 18)
+          .style('fill', function(d) {
              var boxColor = color(d);
              return boxColor;
          });
@@ -273,20 +300,74 @@ function Visify(data) {
     legend.selectAll('text')
         .data(color.domain())
         .enter()
-        .append("text")
+        .append('text')
             .attr('class', 'legendtext')
             .style('text-anchor', 'left')
-            .attr("x", 20+ plotWidth*.01)
-            .attr("y", function(d, i){ return (i * 20)+13;})
+            .attr('x', 20+ plotWidth*.01)
+            .attr('y', function(d, i){ return (i * 20)+13;})
             .text(function(d){
                 var catLabel = d;
                 return [catLabel];
             })
-            .attr("fill", "black")
+            .attr('fill', 'black')
             .style('opacity', 1);
 //****************** </LEGEND TIME> ******************
 //****************************************************
 
 
-  console.log()
+
+
+
+
+
+
+
+//****************** <BUTTON TIME> ******************
+//****************************************************
+    var buttons = svg.append('g')
+//                    .attr('transform','translate('+ plotWidth*.8 + ',50)')
+                    .attr('id', 'button-time');
+
+    var selectors = [{'Label':'CBO Spending'},
+            {'Label':'CBO Revenues'},
+            {'Label':'CBO Debt'},
+            {'Label':'Overview'}];
+
+
+    function inputChange (d) {
+        var inputValue = d.Label;
+
+        if (inputValue === 'CBO Spending')
+            {changeChart(d, inputValue);}
+
+        else if (inputValue === 'CBO Revenues')
+            {changeChart(d, inputValue);}
+
+        else if (inputValue === 'CBO Debt')
+            {changeChart(d, inputValue);}
+
+        else if (inputValue === 'Overview')
+            {changeChart(d, inputValue);};
+    }
+
+    function changeChart (d, inputValue) {
+        console.log(d, inputValue)
+        Visify(allData, inputValue);
+    }
+
+    buttons.selectAll('text')
+            .data(selectors).enter()
+            .append('text')
+                .text(d => d.Label)
+                .attr('class', 'button-text')
+                .style('text-anchor', 'left')
+                .attr('x', width*.80)
+                .attr('y', function(d, i){ return ((height*.20)+i * 40);})
+                .attr('fill', 'black')
+                .style('opacity', 1)
+                .on('click', inputChange);
+
+
+    var uniqCats = d3.set(data, function(d) {return d.Category});
+    //console.log(uniqCats)
 }
